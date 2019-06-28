336de623479c88a86b0f0d7ac657c16427b6b7d1
Luis García Castro <luis.garcia@von-ds.com>
2019-06-28 16:19:11 -0500

    - Added multilanguage support - Marked variables as deprecated

--- src/lib/directives/validation.constants.ts
+++ src/lib/directives/validation.constants.ts
@@ -1,6 +1,27 @@
 export const VALIDATION_MESSAGES: any = {
   ngModelRequired: 'You need to add [(ngModel)] into the element',
+  EN: {
+    requiredMessage: 'The field \'${name}\' is required'
+  },
+  ES: {
+    requiredMessage: 'El campo \'${name}\' es requerido'
+  },
+  /**
+   * @since v0.0.0
+   * @deprecated
+   * Will be deleted in version 1.0.0. Use location section instead: VALIDATION_MESSAGES.EN.requiredMessage
+   */
   requiredMessage: 'The field ${name} is required',
+  /**
+   * @since v0.0.0
+   * @deprecated
+   * Will be deleted in version 1.0.0. Use location section instead: VALIDATION_MESSAGES.EN.equalToSimpleMessage
+   */
   equalToSimpleMessage: 'The field ${field1} is not equal',
+  /**
+   * @since v0.0.0
+   * @deprecated
+   * Will be deleted in version 1.0.0. Use location section instead: VALIDATION_MESSAGES.EN.equalToComplexMessage
+   */
   equalToComplexMessage: 'The field ${field1} is not equal to ${field2}'
 };
